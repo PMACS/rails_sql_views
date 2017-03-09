8bc0b3a32add0e62b4872c01718ca94cb937555a
Raj Iyengar <rgi@mail.med.upenn.edu>
2017-03-09 11:03:33 -0500

    Prepend Schema Dumper.

--- lib/pmacs-rails_sql_views.rb
+++ lib/pmacs-rails_sql_views.rb
@@ -40,7 +40,7 @@ ActiveRecord::ConnectionAdapters::AbstractAdapter.class_eval do
 end
 
 ActiveRecord::SchemaDumper.class_eval do
-  include RailsSqlViews::SchemaDumper
+  prepend RailsSqlViews::SchemaDumper
 end
 
 RailsSqlViews::Loader.load_extensions
